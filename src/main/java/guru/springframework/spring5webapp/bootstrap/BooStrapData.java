327c192596651a15fe3ba257048c6c5c43f7668b
Felix Barsnick <felix.barsnick@digitaspixelpark.com>
2020-05-25 12:24:00 +0200

    changes 2.18

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BooStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BooStrapData.java
@@ -24,30 +24,33 @@ public class BooStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        Publisher pub1 = new Publisher("Publisher #1", "awesome address #1");
+
+        publisherRepository.save(pub1);
+
         Author dan = new Author("Dan", "Brown");
         Book b1 = new Book("Illuminati", "123123");
         dan.getBooks().add(b1);
         b1.getAuthors().add(dan);
+        b1.setPublisher(pub1);
+        pub1.getBooks().add(b1);
 
         authorRepository.save(dan);
         bookRepository.save(b1); // save to H2 database
+        publisherRepository.save(pub1);
 
         Author andy = new Author("Andy", "Weir");
         Book b2 = new Book("The Marsian", "456456");
         andy.getBooks().add(b2);
         b2.getAuthors().add(andy);
+        b2.setPublisher(pub1);
+        pub1.getBooks().add(b2);
 
         authorRepository.save(andy);
         bookRepository.save(b2);
+        publisherRepository.save(pub1);
 
         System.out.println("Started in bootstrap\nNumber of books: " + bookRepository.count());
-
-        Publisher pub1 = new Publisher("Publisher #1", "awesome address #1");
-        Publisher pub2 = new Publisher("Publisher #2", "awesome address #2");
-
-        publisherRepository.save(pub1);
-        publisherRepository.save(pub2);
-
-        System.out.println("Number of publishers: " + publisherRepository.count());
+        System.out.println("Number of publishers: " + publisherRepository.count() + " number of books: " + pub1.getBooks().size());
     }
 }
--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -13,6 +13,9 @@ public class Book { // book POJO class fur use with JPA
     private String title;
     private String isbn;
 
+    @ManyToOne
+    private Publisher publisher;
+
     @ManyToMany
     @JoinTable(name = "author_book",
             joinColumns = @JoinColumn(name = "book_id"),
@@ -82,5 +85,13 @@ public class Book { // book POJO class fur use with JPA
                 ", isbn='" + isbn + '\'' +
                 ", authors=" + authors +
                 '}';
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 }
